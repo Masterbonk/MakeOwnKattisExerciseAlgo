e81524babf0e34060a592303ba84a500c7024abb
Adrian <adrianjuul123@gmail.com>
2025-04-25 13:17:58 +0200

    Made the validate.py file

--- OwnKattis/talisfireball/input_validators/validate.py
+++ OwnKattis/talisfireball/input_validators/validate.py
@@ -2,20 +2,38 @@
 import sys
 import re
 
-for _ in range(2):
-    line = sys.stdin.readline() #Reads the line
-    if not re.match(r"(0|([1-9][0-9]*))\n", line): #Checks for actual numbers
+def checkGroup():
+    n = sys.stdin.readline() #Reads the line
+    if not re.match(r"(0|([1-9][0-9]*))\n", n): #Checks for actual numbers
         print("first")
         sys.exit(43) #fail
-    try:
-        x = int(line) #Turns the line(string) into an int
-
-        if not 0 <= x <= 100: #Checks the range
+    else:
+        try:
+            n = int(n) #Turns the line(string) into an int
+        except ValueError:
             print("second")
             sys.exit(43) #fail
-    except ValueError:
-        print("third")
-        sys.exit(43) #fail
+
+    for _ in range(n):
+        line = sys.stdin.readline()
+        if not re.match(r"(0|([1-9][0-9]*)) (0|([1-9][0-9]*))\n", line): #Checks for actual numbers in the form of x y
+            print("third")
+            sys.exit(43) #fail
+        try:
+            x,y = map(int,line.strip().split(" ")) #Turns the line(string) into an int
+
+            if not 0 <= x <= 100000: #Checks the range
+                print("fourth")
+                sys.exit(43) #fail
+            if not 0 <= y <= 100000: #Checks the range
+                print("fifth")
+                sys.exit(43) #fail
+        except ValueError:
+            print("sixth")
+            sys.exit(43) #fail
+
+checkGroup()
+checkGroup()
 
 if sys.stdin.readline() != "": #Checks that there are only the desired amount of inputs
     sys.exit(43) #fail
