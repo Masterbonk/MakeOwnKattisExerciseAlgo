2735a86195ee2100436f5f2c8124667fa32dd99f
KampJN <josefinekampn@gmail.com>
2025-05-09 12:41:48 +0200

    Added files for Counting Subsequences

---
+++ seperateExercises/countingSubsequences/CountingSubsequences.py
@@ -0,0 +1,79 @@
+##Sums up the array -   runtime: O(n)
+def sum_arr(arr):
+    sum = 0
+    for i in arr:
+        sum += int(i)
+    return sum
+
+##Generates all subsets
+def get_subsets(arr):
+    subsets = [[]] ##Necessary to keep track of the subsets
+    indexes = [[]]
+    interesting = [[]] ##For the ones that sum to 47
+    int_index = [[]]
+    for elem in arr:
+        for i in range(len(subsets)):
+            subset = subsets[i] + [elem]
+            index = indexes[i] + [arr.index(elem)]
+            subsets.append(subset)
+            indexes.append(index)
+            if sum_arr(subset) == 47:
+                interesting.append(subset)
+                int_index.append(index)
+    return interesting[1:], int_index ##To remove the empty set at the start
+
+def rec(arr, i, subset, subsets):
+    if i == len(subsets):
+        subsets.append(subset.copy())
+    else:
+        rec(arr, i + 1, subset+arr[i], subsets)
+        rec(arr, i + 1, subsets[i], subsets)
+
+def alt_get_subsets(arr):
+    subsets = []
+    subset = []
+    rec(arr, 0, subset, subsets)
+    return subsets
+
+
+def clean_up(origin, arr):
+    counter = 0
+    check = [False*origin]
+    for i in arr:
+        if check[i] == False:
+            check[i] = True
+
+
+##Make function to clean up and make sure numbers don't repeat
+##Cleans out sets with duplicate entries
+
+##---------------------------------------------------------------
+cases = int(input())
+
+for i in range(cases):
+    input()  #To deal with the line before each case
+    amount = int(input())
+    lst = input().strip().split()
+
+    ##TESTS
+    ##print(amount)
+    ##print(lst)
+    ##print(sum_arr(lst))
+    interesting, _ = get_subsets(lst)
+    print(interesting)
+    print(len(interesting))
+
+    inter = alt_get_subsets(interesting)
+
+
+##Sort the list. If there are negative, there can be above 47.
+##Remove numbers from list/stack when used
+##Instead of removing, use accumilators?
+##Sum all of them (p.84), then find how many numbers have %47 == 0
+##Use a segment tree to make the above faster
+
+
+##--IDEAS--
+##Sort the arrar
+##Start from middle
+##if the numbers do not add up, go a step up or down
---
+++ seperateExercises/countingSubsequences/InputGeneration.py
@@ -0,0 +1,4 @@
+from random import randint
+
+for i in range(int(1000000)):
+    print(randint(-20000, 20000), end=" ")
