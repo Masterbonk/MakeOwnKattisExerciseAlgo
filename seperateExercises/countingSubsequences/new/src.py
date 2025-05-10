240ace1fdb60a34f2e24024476f12a0251b6ba41
KampJN <josefinekampn@gmail.com>
2025-05-10 09:46:59 +0200

    Added notes and code to CountSub

--- seperateExercises/countingSubsequences/new/src.py
+++ seperateExercises/countingSubsequences/new/src.py
@@ -1,5 +1,22 @@
+##Sums the given array
+def sum_arr(arr):
+    sum = 0
+    for i in arr:
+        sum += int(i)
+    return sum
 
-
+##Fins subsequences in the given array
+def find_subsequences(arr, s, e):
+    counter = 0
+    sum = sum_arr(arr[s:e])
+    if (sum < 47):
+        find_subsequences(arr, s, e+1)
+    if (sum == 47):
+        counter += 1
+        find_subsequences(arr, s+1, e)
+    if (sum > 47):
+        find_subsequences(arr, s+1, e)
+    return counter
 
 ##----------------------------------------------------------------
 cases = int(input())
