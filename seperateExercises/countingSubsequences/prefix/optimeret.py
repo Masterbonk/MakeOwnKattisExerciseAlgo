87e5e865202e50b073b61323bfe5ae84c2585c8f
KampJN <josefinekampn@gmail.com>
2025-05-10 18:43:13 +0200

    Made progress

--- seperateExercises/countingSubsequences/prefix/optimeret.py
+++ seperateExercises/countingSubsequences/prefix/optimeret.py
@@ -1,3 +1,16 @@
+#Binary search tree
+def search(arr, goal):
+    counter = 0
+    if arr[round(len(arr)/2)] < goal:
+        search(arr[:len(arr)/2])
+    elif arr[round(len(arr)/2)] > goal:
+        search(arr[len(arr)/2:])
+    elif arr[round(len(arr)/2)] == goal:
+        #Make a function that goes backwards until it comes across something that is not goal
+        counter += 1
+    return counter
+
+#----------------------------------------------
 
 #Makes a prefix sum array of the given array
 def make_prefix_sum_arr(arr):
@@ -15,14 +28,24 @@ def make_prefix_sum_arr(arr):
     return prefix, counter
 
 #Counts the subsequences that sum to 47
-def count_subsequences(arr):
+# def count_subsequences(arr):
+#     counter = 0
+#     for i in range(len(arr)):
+#         if arr[i] == 47:
+#             counter += 1
+#         for j in range(i+1,len(arr)):
+#             if (arr[j] - arr[i]) == 47:
+#                 counter += 1
+#     return counter
+
+def count_subsequences(order, arr):
     counter = 0
-    for i in range(len(arr)):
-        if arr[i] == 47:
-            counter += 1
-        for j in range(i+1,len(arr)):
-            if (arr[j] - arr[i]) == 47:
-                counter += 1
+    for i in order:
+        c = search(arr[:i], i+47)
+        if c > 0:
+            counter += c
+        else:
+            continue
     return counter
 
 #----------------------------------------------------------------
@@ -33,5 +56,6 @@ for i in range(cases):
     amount = int(input())
     lst = input().strip().split()
     psa, counter = make_prefix_sum_arr(lst)
+    sort = sorted(psa)
     #print(psa)
-    print(count_subsequences(psa) + counter)
+    print(count_subsequences(psa, sort) + counter)
