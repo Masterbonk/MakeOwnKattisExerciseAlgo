11f25a2f2144b3d3f608ac17dc4c25741d95212d
KampJN <josefinekampn@gmail.com>
2025-05-10 12:20:06 +0200

    Worked on CountSub

--- seperateExercises/countingSubsequences/new/src.py
+++ seperateExercises/countingSubsequences/new/src.py
@@ -1,3 +1,8 @@
+import sys
+
+sys.setrecursionlimit(10**6) #This is to deal with the recursionerror. The normal limit is too small to deal with our input size
+
+
 ##Sums the given array
 def sum_arr(arr):
     sum = 0
@@ -7,31 +12,32 @@ def sum_arr(arr):
 
 ##OLD
 ##Fins subsequences in the given array
-def odl_find_subsequences(arr, s, e, counter):
+def find_subsequences(arr, s, e, counter, end):
     sum = sum_arr(arr[s:e])
     if (e-1 == len(arr) and len(arr[s:e]) != 1 and sum == 47):
         counter =+ 1
-        find_subsequences(arr, (s+1), e, counter)
+        find_subsequences(arr, (s+1), e, counter, end)
     elif (e-1 == len(arr) and len(arr[s:e]) != 1):
-        find_subsequences(arr, (s+1), e, counter)
+        find_subsequences(arr, (s+1), e, counter, end)
     elif (e-1 == len(arr) and len(arr[s:e]) == 1):
         return counter
     elif (len(arr[s:e]) == 1 and sum > 47):
-        find_subsequences(arr, s, (e+1), counter)
+        find_subsequences(arr, s, (e+1), counter, end)
     elif (sum < 47):
-        find_subsequences(arr, s, (e+1), counter)
+        find_subsequences(arr, s, (e+1), counter, end)
     elif (sum == 47):
         counter += 1
-        find_subsequences(arr, (s+1), e, counter)
+        find_subsequences(arr, (s+1), e, counter, end)
     elif (sum > 47):
-        find_subsequences(arr, (s+1), e, counter)
+        find_subsequences(arr, (s+1), e, counter, end)
     return counter
 
 ##NEW
 #Finds subsequences
-def find_subsequences(arr):
+def new_find_subsequences(arr):
     s = 0
     e = 1
+    counter = 0
     while True:
         sum = sum_arr(arr[s:e])
         if (e-1 == len(arr) and len(arr[s:e]) != 1 and sum == 47):
@@ -59,4 +65,5 @@ for i in range(cases):
     input()  #To deal with the line before each case
     amount = int(input())
     lst = input().strip().split()
-    print(find_subsequences(lst))
+    print(find_subsequences(lst, 0, 1, 0, amount))
+    #print(new_find_subsequences(lst))
