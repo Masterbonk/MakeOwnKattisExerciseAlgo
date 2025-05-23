dc2077b886ab609f556f3115da3cd4b0f45def25
KampJN <josefinekampn@gmail.com>
2025-05-23 11:15:32 +0200

    Progress

--- seperateExercises/countingSubsequences/prefix/opti.py
+++ seperateExercises/countingSubsequences/prefix/opti.py
@@ -3,7 +3,8 @@ from collections import defaultdict
 #Makes a prefix sum array of the given array
 def make_prefix_sum_arr(arr):
     prefix = [0]*len(arr)
-    tracker = defaultdict(0)
+    tracker = defaultdict(int)
+    tracker[0] = 1
     for i in range(len(arr)):
         if i == 0:
             tracker[(int(arr[i]))] += 1
@@ -17,8 +18,10 @@ def make_prefix_sum_arr(arr):
 def count_subsequences(arr, tracker):
     counter = 0
     for i in range(len(arr)):
+        if arr[i] in tracker:
+            tracker[arr[i]] -= 1
         if arr[i] + 47 in tracker:
-            counter += tracker[arr[i]]
+            counter += tracker.pop(arr[i]+47)
     return counter
 
 #----------------------------------------------------------------
@@ -30,4 +33,5 @@ for i in range(cases):
     lst = input().strip().split()
     psa, tracker = make_prefix_sum_arr(lst)
     #print(psa)
+    #print(tracker)
     print(count_subsequences(psa, tracker))
