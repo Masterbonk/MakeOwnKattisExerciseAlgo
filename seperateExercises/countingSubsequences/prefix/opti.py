37cf540f4a695a0a98c61d4a23aa4c98a80a11d8
Adrian <adrianjuul123@gmail.com>
2025-05-23 12:25:55 +0200

    WHY

--- seperateExercises/countingSubsequences/prefix/opti.py
+++ seperateExercises/countingSubsequences/prefix/opti.py
@@ -3,26 +3,28 @@ from collections import defaultdict
 #Makes a prefix sum array of the given array
 def make_prefix_sum_arr(arr):
     prefix = [0]*len(arr)
-    counter = 0
     tracker = defaultdict(int)
     tracker[0] = 1
+
     for i in range(len(arr)):
+       
         if i == 0:
             tracker[(int(arr[i]))] += 1
             prefix[i] = (int(arr[i]))
         else:
             tracker[int(arr[i]) + int(prefix[i-1])] += 1
-            prefix[i] = (int(arr[i]) + int(prefix[i-1]))
-    return prefix, tracker, counter
+            prefix[i] = (int(arr[i]) + int(prefix[i-1])) 
+            
+    return prefix, tracker
 
 #Counts the subsequences that sum to 47
-def count_subsequences(arr, tracker):
+def count_subsequences(prefix, tracker):
     counter = 0
-    for i in range(len(arr)):
-        if arr[i] + 47 in tracker:
-            counter += tracker.pop(arr[i]+47)
-        if arr[i] in tracker:
-            tracker[arr[i]] -= 1
+    for i in range(len(prefix)):
+        if prefix[i] + 47 in tracker:
+            counter += tracker[prefix[i]+47]#*tracker[prefix[i]]#tracker.pop(arr[i]+47)*tracker[arr[i]]
+        if prefix[i] in tracker:
+            tracker[prefix[i]] -= 1
     return counter
 
 #----------------------------------------------------------------
@@ -32,7 +34,7 @@ for i in range(cases):
     input()  #To deal with the line before each case
     amount = int(input())
     lst = input().strip().split()
-    psa, tracker, counter = make_prefix_sum_arr(lst)
+    prefix, tracker = make_prefix_sum_arr(lst)
     #print(psa)
     #print(tracker)
-    print(count_subsequences(psa, tracker) + counter)
+    print(count_subsequences(prefix, tracker))
